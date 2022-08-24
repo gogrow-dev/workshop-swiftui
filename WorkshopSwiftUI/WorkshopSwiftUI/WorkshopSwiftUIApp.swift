ee7af959764055bf75a8022625d03a676ffdbb72
Nicolas Cobelo <>
2022-08-24 17:16:42 -0300

    Created Xcode project

---
+++ WorkshopSwiftUI/WorkshopSwiftUI/WorkshopSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopSwiftUIApp.swift
+//  WorkshopSwiftUI
+//
+//  Created by Nicolas Cobelo on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
